68fca9ad492f7c82899e250f30a4f4621c020b8e
Alex Shumilov <ashumilov@pivotal.io>
2016-08-24 14:13:04 -0400

    fix context file reference

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath*:META-INF/integration.xml"})
+@ImportResource({"classpath*:META-INF/integration-context.xml"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
